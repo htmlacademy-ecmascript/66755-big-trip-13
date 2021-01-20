4cabd99d4194130e3bb09efb079272d52c9cc411
Denis Gorshkov <satrasmatrassatras@gmail.com>
2021-01-20 18:15:04 +0300

    Правил toCapitalize под линтер

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,7 @@ const isEscape = (event) => event.code === ESCAPE;
 const isMainClick = (event) => event.button === MAIN_CLICK;
 const toCapitalize = (s) => {
   const firstChar = s.slice(0, 1).toUpperCase();
-  const lastChars = [...s].slice(1).map(x => x.toLowerCase()).join('');
+  const lastChars = [...s].slice(1).map((x) => x.toLowerCase()).join(``);
   return `${firstChar}${lastChars}`;
 };
 
