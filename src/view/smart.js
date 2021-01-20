1fb7d655ea2b602bfa626d12ca742b040b32bfc2
Denis Gorshkov <satrasmatrassatras@gmail.com>
2021-01-20 17:59:17 +0300

    Исправляет ссылки на относительные пути

---
+++ src/view/smart.js
@@ -0,0 +1,42 @@
+import AbstractView from "./abstract";
+
+export default class SmartView extends AbstractView {
+  constructor() {
+    super();
+    this._data = {};
+
+    if (new.target === SmartView) {
+      throw new Error(`Cannot create instance of Smart class`);
+    }
+  }
+
+  updateData(update, justDataUpdating = false) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign({}, this._data, update);
+
+    if (justDataUpdating) {
+      return;
+    }
+
+    this.updateElement();
+  }
+
+  updateElement() {
+    let prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, prevElement);
+
+    this.restoreHandlers();
+  }
+
+  restoreHandlers() {
+    throw new Error('restoreHandlers is not realized');
+  }
+}
